41e5a5292ab089730445b3b82e653d35d265f71a
KelvinJPS <kelvinjps10@gmail.com>
2022-06-07 10:08:43 -0500

    Block apps for a certain time

--- To_do_list_WinUI3/Views/Focus_Task.xaml.cs
+++ To_do_list_WinUI3/Views/Focus_Task.xaml.cs
@@ -3,6 +3,8 @@ using Microsoft.UI.Xaml.Controls;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Windows.System;
 using Windows.System.Diagnostics;
@@ -16,7 +18,7 @@ namespace To_do_list_WinUI3.Views
     /// </summary>
     public sealed partial class Focus_Task : Page
     {
-     
+        List<Process> UsefulApps = new List<Process>();
         public Focus_Task()
         {
             this.InitializeComponent();
@@ -34,11 +36,74 @@ namespace To_do_list_WinUI3.Views
                 if (!String.IsNullOrEmpty(p.MainWindowTitle))
                 {
                     processwithwindow.Add(p);
-                    Debug.WriteLine(p.ProcessName);
+                    
                 }
             }
             return processwithwindow;
 
+        }
+
+        private void Add_app_button_Click(object sender, RoutedEventArgs e)
+        {
+            if (ListBox_Apps.SelectedItem != null)
+            {
+                UsefulApps.Add((ListBox_Apps.SelectedItem as Process));
+            }
+            
+        }
+
+        private async void Start_Click(object sender, RoutedEventArgs e)
+        {
+            int hours;
+            int minutes;
+            int M = 0;
+
+            int.TryParse(Hours_Combobox.Text, out hours);
+            int.TryParse(Minutes_combobox.Text, out minutes);
+
+            List<Process> processwithwindow = new List<Process>();
+
+            Task timer = new Task(() =>
+            {
+                for (M = 0; M <= minutes; M++)
+                {                 
+                    Thread.Sleep(minutes * 10000);
+
+                }
+
+            });
+
+            Task Blocker = new Task(() =>
+            {
+                while (M != minutes)
+                {
+                    processwithwindow = GetProcessesWithWindow();
+
+                    foreach (Process p in processwithwindow)
+                    {
+
+                        if (UsefulApps.Any(program => program.Id == p.Id) == false)
+                        {
+                            p.Kill();
+                        }
+
+                    }
+                };
+            }
+                );
+
+            timer.Start();
+            Blocker.Start();
+
+
+        }
+
+        private void Delete_apps_button_Click(object sender, RoutedEventArgs e)
+        {
+            if (ListBox_Apps.SelectedItem != null)
+            {
+                UsefulApps.Remove((ListBox_Apps.SelectedItem as Process));
+            }
         }
     }
 }
